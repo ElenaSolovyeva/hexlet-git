93922b3063f4219ebce3c256d2534d26a92f6615
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2024-03-12 16:23:23 +0100

    Dependencies

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,9 @@
 package io.hexlet;
+import org.apache.commons.lang3.StringUtils;
 
-public class One {
+public class One {    
     public static void main(String[] args) {
-        Two.greeting();
+       String result = StringUtils.reverse("Java for Brave");
+	   System.out.println(result);
     }
 }
-
